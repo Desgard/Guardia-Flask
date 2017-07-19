9cacf2a6e1e152ff35c61460c71f00fc8c02d4bf
Desgard_Duan <gua@desgard.com>
2017-07-19 21:03:29 +0800

    Add admin for db model

--- manage.py
+++ manage.py
@@ -5,11 +5,17 @@ from app import create_app, db
 from app.models import User, Article, Category
 from flask_script import Manager, Shell
 from flask_migrate import Migrate, MigrateCommand
+from flask_admin import Admin
+from flask_admin.contrib.sqla import ModelView
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 manager = Manager(app)
 migrate = Migrate(app, db)
 
+admin = Admin(app)
+admin.add_view(ModelView(User, db.session))
+admin.add_view(ModelView(Article, db.session))
+admin.add_view(ModelView(Category, db.session))
 
 def make_shell_context():
     return dict(app = app, db = db, User = User, Article = Article, Category = Category)
